470c90e5c35d2475cd742ecd69eef219859524f7
Rafael Agostini Vieira <agostini_rafa@hotmail.com>
2021-10-04 01:39:44 -0300

    SoftFinance.API

---
+++ SoftFinance.Api/Controllers/TaxaJurosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoftFinance.Api.Services;
+
+namespace SoftFinance.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxaJurosController : ControllerBase
+    {
+        private readonly ITaxaJurosService _taxaJurosService;
+
+        public TaxaJurosController(ITaxaJurosService taxaJurosService)
+        {
+            _taxaJurosService = taxaJurosService;
+        }
+
+        [HttpGet, Route("taxajuros")]
+        public IActionResult Get()
+        {
+            var response = _taxaJurosService.Get();
+            if(!response.Success) {
+                return BadRequest(response);
+            };
+            return Ok(response);
+        }
+    }
+}
